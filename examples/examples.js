54e65a377593deefe4c676167057b2b4018bf662
maxired <maxired@maxired-VirtualBox.(none)>
2012-09-26 01:14:00 +0200

    add a selec box

--- examples/examples.js
+++ examples/examples.js
@@ -1,9 +1,24 @@
 $(function(){
+
+
+var data = ["#","#Hello","#koi","#moi"];
+
+$("select.first").hide();
+
+$("select.first").change(function(){
+console.log("selected");
+$("textarea.first").val( $("textarea.first").val() + $("select.first").val().substr(1));
+});
 $("textarea.first").keypress(function(e){
 setTimeout(function(){
 var value = e.currentTarget.value;
 if(value.lastIndexOf("#")>-1){
-alert("toto");
+$("select.first").html("");
+data.forEach(function(el){
+$("select.first").append("<option>"+el+"</option>");
+});
+
+$("select.first").show();
 }
 console.log(e.currentTarget.value );
 },0);
